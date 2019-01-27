b84316cdc5e25b8be563b2451624f5df84ed5a3b
slavvok <slavvok@gmail.com>
2019-01-28 02:47:14 +0300

    Works

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views
 
-<<<<<<< HEAD
-handler404 = views.handler404
-=======
 handler404 = 'views.handler404'
->>>>>>> 01d4c024dddf9375ac7ccc3c3b76d69333713388
 
 urlpatterns = [
     path('', views.index),
